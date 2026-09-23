35a2ae7b97f0c78631617c87ef7ef929b78629db
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-23 11:50:43 -0300

    push

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 [Serializable]
 public class InputEvents
@@ -14,4 +15,43 @@ public class InputEvents
 public class InputManager : MonoBehaviour
 {
     public InputEvents[] AllEvents;
+
+    public static Action onMove; //PlayerManager
+    public static Action onInteract; //InteractableObject
+    public static Action onPause; //UIManager
+
+    public InputAction playerMovement;
+    public InputAction interactable;
+    public InputAction pause;
+
+    public void Start()
+    {
+
+        //playerMovement.performed.AddListener(() => AllEvents[1].InputEvent.Invoke());
+        //or something like this
+    }
+
+    public void OnMove()
+    {
+        if (onMove != null)
+        {
+            onMove.Invoke();
+        }
+    }
+    public void OnInteract()
+    {
+        if (onInteract != null)
+        {
+            onInteract.Invoke();
+        }
+    }
+
+    public void OnPause()
+    {
+        if (onPause != null)
+        {
+            onPause.Invoke();
+        }
+    }
+
 }
